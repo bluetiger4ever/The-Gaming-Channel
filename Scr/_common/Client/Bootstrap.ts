81b29baf03b9be12f40177c67ef5f5234cefde3d
Depressed hacker <81526718+bluetiger4ever@users.noreply.github.com>
2021-11-18 09:14:11 -0500

    Create Bootstrap.ts

---
+++ Scr/_common/Client/Bootstrap.ts
@@ -0,0 +1,12 @@
+import { VuexStore } from '../../utils/vuex';
+import { initClientApiInterceptors } from './api/api.service';
+import { ClientUpdater } from './client-updater.service';
+import { Client } from './client.service';
+import { ClientUser } from './user/user.service';
+
+export function bootstrapCommonClient(sectionStore: VuexStore) {
+	initClientApiInterceptors();
+	Client.init();
+	ClientUser.init(sectionStore);
+	ClientUpdater.init();
+}
