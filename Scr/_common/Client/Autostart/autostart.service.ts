91ac9853773ee4648fe526dce3bb34e4d6ce64d8
Depressed hacker <81526718+bluetiger4ever@users.noreply.github.com>
2021-11-18 08:59:29 -0500

    Create autostart.service.ts

---
+++ Scr/_common/Client/Autostart/autostart.service.ts
@@ -0,0 +1,49 @@
+import { Autostarter } from 'client-voodoo';
+import * as path from 'path';
+import { Device } from '../../device/device.service';
+import { SettingAutostartClient } from '../../settings/settings.service';
+import { Client } from '../client.service';
+
+export class ClientAutoStart {
+	static init() {
+		if (SettingAutostartClient.get()) {
+			this.set();
+		}
+	}
+
+	static get canAutoStart() {
+		if (GC_IS_WATCHING) {
+			return false;
+		}
+
+		return Device.os() === 'windows';
+	}
+
+	static async set() {
+		if (!this.canAutoStart) {
+			return;
+		}
+
+		return Autostarter.set(path.join(Client.coltronDir, 'GamingchannelClient.exe'), [
+			'run',
+			'--',
+			'--silent-start',
+		]);
+	}
+
+	static async clear() {
+		if (!this.canAutoStart) {
+			return;
+		}
+
+		return Autostarter.unset();
+	}
+
+	static async check() {
+		if (!this.canAutoStart) {
+			return;
+		}
+
+		return Autostarter.isset();
+	}
+}
