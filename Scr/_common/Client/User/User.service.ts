8ad06c30d982ed0da13ab29cc267481e3fb1b188
Depressed hacker <81526718+bluetiger4ever@users.noreply.github.com>
2021-11-18 09:13:36 -0500

    Create User.service.ts

---
+++ Scr/_common/Client/User/User.service.ts
@@ -0,0 +1,53 @@
+import { VuexStore } from '../../../utils/vuex';
+import { Environment } from '../../environment/environment.service';
+import { Navigate } from '../../navigate/navigate.service';
+import { AppStore } from '../../store/app-store';
+import { User } from '../../user/user.model';
+
+// So that this can be pulled into any section and not rely on the main "app" store, we manually
+// attach this so we know it exists.
+type Store_ = VuexStore<{ app: AppStore }>;
+
+export class ClientUser {
+	static init(store: Store_) {
+		// We bootstrap the client with the previously stored user if there was any.
+		// This way they can access client offline with their previous user.
+		const localUser = localStorage.getItem('user');
+		if (localUser) {
+			const user = new User(JSON.parse(localUser));
+			store.commit('app/setUser', user);
+		} else if (Navigate.currentClientSection !== 'auth') {
+			// Must be logged in to use client.
+			this.authRedirect();
+		}
+
+		// When the app user changes in the store, freeze it into local storage so we can bootstrap
+		// from that next client launch (before any payload response).
+		store.watch(
+			state => state.app.user,
+			user => {
+				if (user) {
+					localStorage.setItem('user', JSON.stringify(user));
+				} else {
+					localStorage.removeItem('user');
+
+					// If they're logged out in client, we have to force to auth page..
+					this.authRedirect();
+				}
+			}
+		);
+	}
+
+	private static authRedirect() {
+		// TODO: This is a hack to fix redirect loop between the client sections and the auth section.
+		// Since redirecting with window.location.href isnt really synchronous theres a race condition
+		// between the time user service figures out it has no user and redirects to auth
+		// and the init logic in client service to redirect to downgrade section.
+		//
+		// This hack will not hold if we have other sections under the 'client' section that need to redirect to auth if not logged in.
+		const fromSection = Navigate.currentClientSection;
+		if (!Navigate.isRedirecting && (!fromSection || fromSection !== 'client')) {
+			Navigate.goto(Environment.authBaseUrl + '/login');
+		}
+	}
+}
