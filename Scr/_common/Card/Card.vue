3b990fcab93d9a1b28a25d94aefee97ce1b4dd27
Depressed hacker <81526718+bluetiger4ever@users.noreply.github.com>
2021-11-18 08:49:15 -0500

    Create Card.vue

---
+++ Scr/_common/Card/Card.vue
@@ -0,0 +1,19 @@
+<script lang="ts" src="./card"></script>
+
+<template>
+	<div
+		class="card fill-darkest"
+		:class="{
+			'card-disabled': isDisabled,
+			'card-expandable': isExpandable,
+			'card-draggable': isDraggable,
+			'card-expandable-padding': forceExpandablePadding,
+		}"
+	>
+		<div v-if="isExpandable" class="card-chevron">
+			<app-colticon :icon="isExpanded ? 'chevron-down' : 'chevron-right'" />
+		</div>
+
+		<slot />
+	</div>
+</template>
