4609e8697a33d79ba28b347de2feae69a5a6056f
Depressed hacker <81526718+bluetiger4ever@users.noreply.github.com>
2021-11-18 09:08:51 -0500

    Create shortcut.service.ts

---
+++ Scr/_common/Client/Shortcut/shortcut.service.ts
@@ -0,0 +1,35 @@
+import { Shortcut } from 'client-voodoo';
+import * as path from 'path';
+import { Device } from '../../device/device.service';
+import { Client } from '../client.service';
+
+export class ClientShortcut {
+	static get supportsShortcuts() {
+		if (GC_IS_WATCHING) {
+			return false;
+		}
+		// We just make ".desktop" entries for linux at the moment.
+		// This way it's easier to launch them.
+		return Device.os() === 'linux';
+	}
+
+	static create() {
+		if (!this.supportsShortcuts) {
+			return;
+		}
+
+		return Shortcut.create(
+			path.join(Client.joltronDir, 'gaming-channel-client'),
+			// Path is absolute, so we make it relative to get the resolve working.
+			path.resolve(require('../../../static-assets/client/icon-256x256.png').substr(1))
+		);
+	}
+
+	static remove() {
+		if (!this.supportsShortcuts) {
+			return;
+		}
+
+		return Shortcut.remove();
+	}
+}
