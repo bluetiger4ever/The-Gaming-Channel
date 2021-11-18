cd94b1490660c87ccf5ae02748edd81be3914e08
Depressed hacker <81526718+bluetiger4ever@users.noreply.github.com>
2021-11-18 09:04:45 -0500

    Create History-navigator.vue

---
+++ Scr/_common/Client/History-navigator/History-navigator.vue
@@ -0,0 +1,22 @@
+<template>
+	<span>
+		<a
+			v-if="HistoryNavigator.canGoBack"
+			class="navbar-item"
+			title="Go back"
+			@click="HistoryNavigator.back()"
+		>
+			<app-colticon icon="chevron-left" />
+		</a>
+		<a
+			v-if="HistoryNavigator.canGoForward"
+			class="navbar-item"
+			title="Go forward"
+			@click="HistoryNavigator.forward()"
+		>
+			<app-colticon icon="chevron-right" />
+		</a>
+	</span>
+</template>
+
+<script lang="ts" src="./history-navigator"></script>
