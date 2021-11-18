e95edaaaca4940466c7417dec9c46c399e3f734f
Depressed hacker <81526718+bluetiger4ever@users.noreply.github.com>
2021-11-18 09:39:10 -0500

    Create Modal.vue

---
+++ Scr/_common/Comment/Modal/Modal.vue
@@ -0,0 +1,27 @@
+<script lang="ts" src="./modal"></script>
+
+<template>
+	<app-modal>
+		<div class="modal-controls">
+			<app-button @click="modal.dismiss()">
+				<translate>Close</translate>
+			</app-button>
+		</div>
+
+		<div class="modal-body">
+			<app-comment-widget
+				:model="model"
+				:autofocus="autofocusAdd"
+				:initial-tab="initialTab"
+				:display-mode="displayMode"
+			/>
+		</div>
+	</app-modal>
+</template>
+
+<style lang="stylus" scoped>
+@import '~styles/variables'
+@import '~styles-lib/mixins'
+>>> .timeline-list-item-split
+	full-bleed()
+</style>
