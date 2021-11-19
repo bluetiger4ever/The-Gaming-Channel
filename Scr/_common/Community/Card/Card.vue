ea334fa97bf817269eca60f1be6efe38572fc3f7
Depressed hacker <81526718+bluetiger4ever@users.noreply.github.com>
2021-11-19 08:34:01 -0500

    Create Card.vue

---
+++ Scr/_common/Community/Card/Card.vue
@@ -0,0 +1,13 @@
+<script lang="ts" src="./card"></script>
+
+<template>
+	<app-community-card-base :community="community" :elevate="elevate" :allow-edit="allowEdit">
+		<template #thumbnail>
+			<router-link :to="community.routeLocation">
+				<app-media-item-backdrop :media-item="community.thumbnail">
+					<app-community-thumbnail-img :community="community" />
+				</app-media-item-backdrop>
+			</router-link>
+		</template>
+	</app-community-card-base>
+</template>
