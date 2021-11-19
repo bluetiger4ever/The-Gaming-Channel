c9cbacc9927a528c4da1b5d138430747401ed030
Depressed hacker <81526718+bluetiger4ever@users.noreply.github.com>
2021-11-19 09:04:33 -0500

    Create Verified-tick.vue

---
+++ Scr/_common/Community/Verified-tick/Verified-tick.vue
@@ -0,0 +1,21 @@
+<template>
+	<app-colticon
+		v-if="community.is_verified"
+		:class="{
+			'-small': this.small,
+		}"
+		icon="verified"
+		:big="big"
+		v-app-tooltip="tooltip"
+	/>
+</template>
+
+<style lang="stylus" scoped>
+@require '~styles/variables'
+$colticon-size = 16px
+.-small
+	margin: 0 2px
+	font-size: $colticon-size * 0.85
+</style>
+
+<script lang="ts" src="./verified-tick"></script>
