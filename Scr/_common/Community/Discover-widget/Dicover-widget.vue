47d45fbd5aac9131afe7cb664d51a39cde78711e
Depressed hacker <81526718+bluetiger4ever@users.noreply.github.com>
2021-11-19 08:48:38 -0500

    Create Dicover-widget.vue

---
+++ Scr/_common/Community/Discover-widget/Dicover-widget.vue
@@ -0,0 +1,32 @@
+<template>
+	<router-link class="-discover" :to="{ name: 'discover.communities' }" v-app-tooltip="tooltip">
+		<app-colticon icon="search" />
+	</router-link>
+</template>
+
+<style lang="stylus" scoped>
+@require '~styles/variables'
+@require '~styles-lib/mixins'
+.-discover
+	pressy()
+	display: flex
+	justify-content: center
+	align-items: center
+	border: $border-width-large dashed
+	border-color: var(--theme-fg-muted)
+	color: var(--theme-fg-muted)
+	border-radius: 100%
+	cursor: pointer
+	position: absolute
+	width: 100%
+	height: 100%
+	outline: 0
+	&:hover
+		border-color: var(--theme-link)
+		color: var(--theme-link)
+.colticon
+	margin: 0 3px
+	font-size: $colticon-size * 1.5
+</style>
+
+<script lang="ts" src="./discover-widget"></script>
