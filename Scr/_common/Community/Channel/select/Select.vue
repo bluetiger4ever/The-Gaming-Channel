c3ba0a912cf83302ba0b6ecca534614f714d951a
Depressed hacker <81526718+bluetiger4ever@users.noreply.github.com>
2021-11-19 08:35:12 -0500

    Create Select.vue

---
+++ Scr/_common/Community/Channel/select/Select.vue
@@ -0,0 +1,14 @@
+<script lang="ts" src="./select"></script>
+
+<template>
+	<div v-if="validChannels.length">
+		<span v-for="channel of validChannels" :key="channel.id">
+			<app-pill
+				:class="{ active: value && value.id === channel.id }"
+				@click="emitInput(channel)"
+			>
+				{{ channel.displayTitle }}
+			</app-pill>
+		</span>
+	</div>
+</template>
