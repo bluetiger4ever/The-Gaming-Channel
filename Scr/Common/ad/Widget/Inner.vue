625ea2a04296e6a8e36c1d29663dc9914d9aae5b
Depressed hacker <81526718+bluetiger4ever@users.noreply.github.com>
2022-03-01 13:59:27 -0500

    Create Inner.vue

---
+++ Scr/Common/ad/Widget/Inner.vue
@@ -0,0 +1,9 @@
+<template>
+	<!-- We completely regenerate it when the slot ID changes -->
+	<div v-if="slotId" :key="slotId" class="ad-widget-inner">
+		<!-- Load the ad component for the currently running ad adapter -->
+		<component :is="adapter.component(adSlot)" :ad-slot="adSlot" :adapter="adapter" />
+	</div>
+</template>
+
+<script lang="ts" src="./inner"></script>
