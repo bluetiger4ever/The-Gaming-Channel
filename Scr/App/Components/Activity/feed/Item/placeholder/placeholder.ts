54902d237750dbc144bb887aca88c97b8d03701c
Depressed hacker <81526718+bluetiger4ever@users.noreply.github.com>
2022-03-14 13:30:27 -0400

    Create placeholder.ts

---
+++ Scr/App/Components/Activity/feed/Item/placeholder/placeholder.ts
@@ -0,0 +1,10 @@
+import AppButtonPlaceholder from '../../../../../../_common/button/placeholder/placeholder.vue';
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component({
+	components: {
+		AppButtonPlaceholder,
+	},
+})
+export default class AppActivityFeedItemPlaceholder extends Vue {}
