ec9035c477473ea8dc5639fabccf52a73cfe7ef0
Depressed hacker <81526718+bluetiger4ever@users.noreply.github.com>
2022-03-14 13:34:46 -0400

    Create New-button.ts

---
+++ Scr/App/Components/Activity/feed/New-button/New-button.ts
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import { Component, Emit } from 'vue-property-decorator';
+
+@Component({})
+export default class AppActivityFeedNewButton extends Vue {
+	@Emit('click') emitClick(_e: Event) {}
+}
