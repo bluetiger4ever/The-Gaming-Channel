f4f7f4daecf581e697e7cf152f64c1ed0979e1ae
Depressed hacker <81526718+bluetiger4ever@users.noreply.github.com>
2022-03-14 13:31:01 -0400

    Create Placeholder.vue

---
+++ Scr/App/Components/Activity/feed/Item/placeholder/Placeholder.vue
@@ -0,0 +1,49 @@
+<script lang="ts" src="./placeholder"></script>
+
+<template>
+	<div class="-container">
+		<div class="-item">
+			<div class="-header">
+				<div class="-header-content">
+					<div class="-header-avatar" />
+
+					<div class="-header-byline">
+						<div class="-header-byline-name">
+							<span class="-placeholder-text" style="width: 100px" />
+						</div>
+					</div>
+				</div>
+				<div class="-header-meta">
+					<span class="-placeholder-small" style="width: 75px" />
+				</div>
+			</div>
+
+			<div class="fireside-post-lead">
+				<span class="-placeholder-text" />
+				<span class="-placeholder-text" />
+				<span class="-placeholder-text" style="width: 40%" />
+			</div>
+
+			<div class="-controls">
+				<app-button-placeholder sparse circle />
+				<app-button-placeholder sparse circle />
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="stylus" scoped>
+@import '../../event-item/event-item'
+@import '~styles-lib/mixins'
+// Gotta override the border hover styling of event items.
+.-item
+	&
+	&:hover
+		border-color: transparent
+		cursor: default
+.-placeholder-text
+	lazy-placeholder-inline()
+.-placeholder-small
+	lazy-placeholder-inline()
+	height: $font-size-small
+</style>
